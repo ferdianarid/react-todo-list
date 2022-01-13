e8746f7e7576a994565fa9219d0755383a976951
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-14 01:48:06 +0700

    activate mode jit tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
+//  Form Components
+import Form from "./components/Form"
+
+// Input App css
+import './App.css'
+
+// Todo List Components
+import TodoList from "./components/TodoList"
+
+// App Components
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// State Input Text
+	const [inputText, setInputText] = React.useState("")
+
+	// State Todos || initial state array
+	const [Todos, setTodos] = React.useState([])
+	return (
+		<div className="App mt-8">
+			{/* Title */}
+			<h1 className="text-5xl font-extrabold text-white text-center">Create Todo List</h1>
+
+			{/* Form Component */}
+			<Form inputText={inputText} todos={Todos} setTodoList={setTodos} changeText={setInputText} />
+
+			{/* Todo List Component */}
+			<TodoList todos={Todos} />
+		</div>
+	)
 }
 
-export default App;
+export default App
