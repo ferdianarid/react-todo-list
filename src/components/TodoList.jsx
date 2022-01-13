ca58ea9d22ff9882792f02107b35c1d5d5284e10
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-14 00:14:59 +0700

    add input and todolist component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoList = () => {
+       return (
+              <React.Fragment>
+                     <div className="todo-container">
+                            <ul className="todo-list"></ul>
+                     </div>
+              </React.Fragment>
+       )
+}
+
+export default TodoList
