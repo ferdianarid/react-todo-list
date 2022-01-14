87d8735bc926a08a0323699c7a78b946ae563a19
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-01-14 11:43:20 +0700

    finished app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 //  Form Components
 import Form from "./components/Form"
@@ -23,19 +23,54 @@ function App() {
 	// State Filtered Todos
 	const [filteredTodos, setFilteredTodos] = React.useState([])
 
+	// Use Side Effect Hook \\ Execute First Time
+	useEffect(() => {
+		GetLocalStorage()
+	}, [])
+
+	// Use Side Effect Hook
+	useEffect(() => {
+		FilterTodosHandler()
+		StoreLocalStorage()
+	}, [Todos, status])
+
 	// Filter todos based condition
 	const FilterTodosHandler = () => {
 		// Check Filter  and getTodos with status completed or not completed
 		switch (status) {
+			// return filtered todos completed 
 			case "completed":
-				setFilteredTodos(todos.filter(todo => todo.completed === "completed"))
+				setFilteredTodos(Todos.filter(todo => todo.completed === true))
 				break
+			// return filtered todos uncompleted
 			case "uncompleted":
-				setFilteredTodos(todos.filter(todo => todo.completed === "uncompleted"))
+				setFilteredTodos(Todos.filter(todo => todo.completed === false))
 				break
+			default:
+				// return filtered todos
+				setFilteredTodos(Todos)
 		}
 	}
-	// continue -> send props status to this component
+
+	// Save Todos to Local Storage
+	const StoreLocalStorage = () => {
+		// Set Todos to Local Storage
+		localStorage.setItem("Todos ", JSON.stringify(Todos))
+	}
+
+	// Get Local Storage Todos
+	const GetLocalStorage = () => {
+		// Check Todos in local storage
+		if (localStorage.getItem("Todos") === null) {
+			// if local storage is empty, return empty array
+			localStorage.setItem("Todos ", JSON.stringify([]))
+		} else {
+			// Set Todos to local storage
+			const LocalTodo = localStorage.getItem("Todos ", JSON.stringify(Todos))
+			console.log(LocalTodo);
+		}
+	}
+
 	return (
 		<div className="App mt-8">
 			{/* Title */}
@@ -45,7 +80,7 @@ function App() {
 			<Form setStatus={setStatus} inputText={inputText} todos={Todos} setTodoList={setTodos} changeText={setInputText} />
 
 			{/* Todo List Component */}
-			<TodoList setTodos={setTodos} todos={Todos} />
+			<TodoList filteredTodos={filteredTodos} setTodos={setTodos} todos={Todos} />
 		</div>
 	)
 }
